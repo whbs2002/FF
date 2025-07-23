67a44e4e3efc80f34f7d11e23ab9c84a41b326f3
whbs2002 <whbs2002@gmail.com>
2025-07-23 16:53:35 -0400

    Worked on par

--- par.py
+++ par.py
@@ -20,7 +20,6 @@ def ith_largest_or_zero(group, i):
         return 0
     return top_i.iloc[-1]
 
-
 # find stats for replacement player
 def replacement_stats(data):
     replacement = {}
@@ -38,27 +37,69 @@ def par(data, replacement):
     data = data.drop(columns=['ppg_replacement']) 
     return data[['player_id', 'player_name','season', 'position', 'ppg', 'par']].sort_values(by=['season', 'par'], ascending=[False, False]).reset_index(drop=True)
 
-def density_plots(yearly,player_identity, ppr=True,years=(2019,2025),limit = 0.0):
-    points = ""
+# Creates density plots for groups of positions
+def density_plots(groups, val_col, file_name):
+    labels = []
+    data = []
+    for name,group in groups:
+        labels.append(name)
+        data.append(group[val_col].to_numpy())
+    print(labels)
+    fig = ff.create_distplot(data,labels,bin_size=10)
+    fig.write_html(file_name)
+    fig.write_image(file_name.replace('.html', '.png'))
+
+# All players over a certain points limit
+def limits_hist(yearly,player_identity, type,years=(2019,2025), limit = 0.0):
     positions = ['QB','WR','RB','TE']
-    if ppr: points = 'fantasy_points_ppr' 
-    else: points = 'fantasy_points'
+    if type == 'ppr': 
+        points = 'fantasy_points_ppr'
+        file_name = 'figures/points_limit_ppr.html' 
+    elif type == 'par':
+        points = 'par'
+        file_name = 'figures/points_limit_par.html'
+    else: 
+        points = 'fantasy_points'
+        file_name = 'figures/points_limit_standard.html'
     year_list = [i for i in range(years[0],years[1])]
     yearly = yearly[yearly['season'].isin(year_list)]
     yearly = yearly[yearly[points]>limit]
-    yearly = yearly.merge(player_identity, on='player_id')
+    yearly = yearly.merge(player_identity, on='player_id',suffixes=('','_x'))
     yearly = yearly[yearly['position'].isin(positions)]
     grouped = yearly.groupby('position')
-    labels = []
-    data = []
-    for name,group in grouped:
-        labels.append(name)
-        data.append(group[points].to_numpy())
-    print(labels)
-    fig = ff.create_distplot(data,labels,bin_size=10)
-    fig.write_html('figures/par_hist.html')
-    fig.write_image('figures/par_hist.png')
+    density_plots(grouped, points, file_name)
 
+# The top n players in a position for a given year
+def top_n_hist(yearly,player_identity,type,years=(2019,2025),n=30):
+    positions = ['QB','WR','RB','TE']
+    if type == 'ppr': 
+        points = 'fantasy_points_ppr'
+        file_name = 'figures/points_n_ppr.html' 
+    elif type == 'par':
+        points = 'par'
+        file_name = 'figures/points_n_par.html'
+    else: 
+        points = 'fantasy_points'
+        file_name = 'figures/points_n_standard.html'
+    year_list = [i for i in range(years[0],years[1])]
+    yearly = yearly[yearly['season'].isin(year_list)]
+    yearly = yearly.merge(player_identity, on='player_id',suffixes=('','_x'))
+    yearly = yearly[yearly['position'].isin(positions)]
+    grouped = yearly.groupby(['position','season'])
+    top = grouped.apply(lambda x: x.nlargest(n,points)).reset_index(drop=True)
+    grouped = top.groupby('position')
+    density_plots(grouped, points, file_name)
+
+def top_rounds_pos(stats,rounds=2,years=(2019,2025)):
+    n = rounds* TEAMS
+    positions = ['QB','WR','RB','TE']
+    stats = stats[stats['season'].isin([i for i in range(years[0],years[1])])]
+    top = stats[stats['position'].isin(positions)]
+    top = top.groupby(['season']).apply(lambda x: x.nlargest(n,'par')).reset_index(drop=True)
+    top = top.groupby(['season'])['position'].value_counts()
+    top = top.reset_index(name='count')
+    fig = px.area(top,x='season',y='count',color='position', line_group='position')
+    fig.write_image('figures/top_rounds_pos.png',width=1400,height=1000)
 
 def main():
     identity, weekly, yearly, overall = load_data()
@@ -72,7 +113,12 @@ def main():
     file = open('data/points_above_replacement.csv', 'w')
     file.write(par_results.to_csv(index=False))
     file.close()
-    density_plots(yearly,identity,ppr=False,limit=100.0)
+    #limits_hist(yearly,identity,type='ppr',limit=17.0)
+    #top_n_hist(yearly,identity,type='ppr')
+    #par_results['par'] = par_results['par']*17.0
+    #limits_hist(par_results,identity,type='par',limit=17.0)
+    #top_n_hist(par_results,identity,type='par')
+    top_rounds_pos(par_results,rounds=2,years=(1999,2025))
 
 
 
